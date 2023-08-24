01d5de0130dfed541b9df81e5e7f211182592d37
CavassinGomes <lucascavassinig@gmail.com>
2023-08-24 12:01:13 -0300

    Serviço e Repositório de cliente e start de agendamento.

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.soulcode.goserviceapp.service;
 import com.soulcode.goserviceapp.domain.Cliente;
 import com.soulcode.goserviceapp.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +12,19 @@ import java.util.Optional;
 public class ClienteService {
     @Autowired
     private ClienteRepository cllienteRepository;
+
+    public Cliente findAuthenticaded(Authentication authentication){
+        if(authentication != null && authentication.isAuthenticated()){
+            Optional <Cliente> cliente = cllienteRepository.findByEmail(authentication.getName());
+            if(cliente.isPresent()){
+                return cliente.get();
+            }else{
+                throw new RuntimeException("Cliente não encontrado.");
+            }
+        }else{
+            throw new RuntimeException("Cliente não autenticado");
+        }
+    }
 
     public Cliente findById(Long id){
         Optional<Cliente> cliente = cllienteRepository.findById(id);
